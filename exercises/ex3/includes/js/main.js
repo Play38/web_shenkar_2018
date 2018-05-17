adb5728695d4652ad28ac9881550eb8013a1c57c
Play38 <ofir.cohen@stu.shenkar.ac.il>
2018-05-17 07:45:40 +0300

    code optimization

--- exercises/ex3/includes/js/main.js
+++ exercises/ex3/includes/js/main.js
@@ -1,30 +1,27 @@
 $(document).ready(function(){
     var originColor;
     var originOpacity;
-    var originHeight = 500;
-    var bottomBox = $(".main_lay3");
-    var bottomHoverBox = $(".main_lay3 div");
     var upperBox =$(".right_side_lay3 div")
-    var box_count = 1;
+    var boxCount = 1;
     var firstCheck = 1;
-    colorChange(originColor, originOpacity, upperBox, bottomHoverBox);
-    createBox(bottomBox, box_count, firstCheck);
+    colorChange(originColor, originOpacity, upperBox);
+    createBox(boxCount, firstCheck);
 })
 
-function createBox(bottomBox, box_count, firstCheck) {
+function createBox(boxCount, firstCheck) {
 	 $(".button").on("click", function () {
-	 	  	 if((box_count % 5) == 0 )
+	 	  	 if((boxCount % 5) == 0 )
             {
             if (firstCheck ==1)
             {
             	firstCheck = 0;
             }
             else {
-            $(bottomBox).css("height", "+=250")
+            $(".main_lay3").css("height", "+=250");
             }
-            box_count = 1;
+            boxCount = 1;
             }
-          	$(bottomBox) .append("<div></div>") .find("div:last") .css("opacity", Math.random() ).hover(function () {
+          	$(".main_lay3") .append("<div></div>") .find("div:last") .css("opacity", Math.random() ).hover(function () {
     originColor = $(this).css("background-color");
     originOpacity = $(this).css("opacity");
     $(this).css({ "background-color": "#ffffff", "opacity": "1" });
@@ -32,13 +29,13 @@ function createBox(bottomBox, box_count, firstCheck) {
     , function () {
     	$(this).css({ "background-color": originColor, "opacity": originOpacity });
    	});
-            box_count++;
+            boxCount++;
     });
 }
 
 
 
-function colorChange(originColor, originOpacity, upperBox, bottomHoverBox) {
+function colorChange(originColor, originOpacity, upperBox) {
 	upperBox.hover(function () {
     originColor = $(this).css("background-color");
     originOpacity = $(this).css("opacity");
