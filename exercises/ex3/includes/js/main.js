fe8b7cedf246ec04d1c428ba67ef893e1d95c1c6
Play38 <ofir.cohen@stu.shenkar.ac.il>
2018-05-18 17:10:07 +0300

    code optimization

--- exercises/ex3/includes/js/main.js
+++ exercises/ex3/includes/js/main.js
@@ -1,39 +1,35 @@
 $(document).ready(function(){
     var originColor;
     var originOpacity;
-    var upperBox =$(".right_side_lay3 div")
     var boxCount = 1;
     var firstCheck = 1;
     var element;
-	bgColor(element);
-    createBox(boxCount, firstCheck, originColor, originOpacity);
+	boxGenerator(element);
+    plusBoxes(boxCount, firstCheck);
 })
-
-$(document).ready(function first4boxes () {
-	  var firstboxes = 0;
-    	while (firstboxes < 4) {
-    bgColor(".right_side_lay3")
+$(document).ready(function first4Boxes () {
+	var firstboxes = 0;
+	while (firstboxes < 4) {
+	boxGenerator(".right_side_lay3");
     firstboxes++;
-}
- });
-
-function createBox(boxCount, firstCheck, originColor, originOpacity) {
+	}
+});
+function plusBoxes(boxCount, firstCheck) {
     $(".button").on("click", function() {
         if ((boxCount % 5) == 0) {
-           if (firstCheck == 1) {
-               firstCheck = 0;
-           } else {
-               $(".main_lay3").css("height", "+=250");
-           }
-           boxCount = 1;
+        	if (firstCheck == 1) {
+            	firstCheck = 0;
+           	} 
+           	else {
+            	$(".main_lay3").css("height", "+=250");
+           	}
+           	boxCount = 1;
         }
-        bgColor(".main_lay3")
-        boxCount++;
+	boxGenerator(".main_lay3");
+	boxCount++;
     });
 }
-
-
-function bgColor(element) {
+function boxGenerator(element) {
     return $(element).append("<div></div>").find("div:last").css("opacity", Math.random()).hover(function() {
         originColor = $(this).css("background-color");
         originOpacity = $(this).css("opacity");
@@ -41,4 +37,4 @@ function bgColor(element) {
     }, function() {
         $(this).css({ "background-color": originColor, "opacity": originOpacity });
     });
-   };
+};
